7687d5893cc966687fbbd35550e989793c01315a
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-10-21 15:39:06 +0100

    add all port properties

--- ComsPortComunicator/ComsPortComunicator/Model/ComPortModel.cs
+++ ComsPortComunicator/ComsPortComunicator/Model/ComPortModel.cs
@@ -3,6 +3,11 @@
     public class ComPortModel : BaseModel
     {
         private string _portName;
+        private string _baudRate;
+        private string _dataBits;
+        private string _stopBits;
+        private string _parity;
+        private string _handShake;
 
         public string PortName
         {
@@ -13,5 +18,56 @@
                 OnPropertyChanged();
             }
         }
+
+        public string BaudRate
+        {
+            get { return _baudRate; }
+            set
+            {
+                _baudRate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DataBits
+        {
+            get { return _dataBits; }
+            set
+            {
+                _dataBits = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string StopBits
+        {
+            get { return _stopBits; }
+            set
+            {
+                _stopBits = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Parity
+        {
+            get { return _parity; }
+            set
+            {
+                _parity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string HandShake
+        {
+            get { return _handShake; }
+            set
+            {
+                _handShake = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
--- ComsPortComunicator/ComsPortComunicator/ViewModel/MainWindowViewModel.cs
+++ ComsPortComunicator/ComsPortComunicator/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
+using System.Windows.Documents;
 using System.Windows.Input;
 using ComsPortComunicator.Command;
 using ComsPortComunicator.Model;
@@ -10,17 +12,90 @@ namespace ComsPortComunicator.ViewModel
     public class MainWindowViewModel : BaseViewModel
     {
         private ObservableCollection<string> _comPortNames = new ObservableCollection<string>();
+        private List<string> _comBaudRates = new List<string>();
+        private List<string> _comDataBits = new List<string>();
+        private List<string> _comStopBits = new List<string>();
+        private List<string> _comParities = new List<string>();
+        private List<string> _comHandShakes = new List<string>();
+
         private ComPortModel _comPort = new ComPortModel();
 
         public MainWindowViewModel()
         {
             InitCommands();
+
+            PopulateLists();
+        }
+
+        private void PopulateLists()
+        {
+            ComBaudRates.Add("300");
+            ComBaudRates.Add("600");
+            ComBaudRates.Add("1200");
+            ComBaudRates.Add("2400");
+            ComBaudRates.Add("9600");
+            ComBaudRates.Add("14400");
+            ComBaudRates.Add("19200");
+            ComBaudRates.Add("38400");
+            ComBaudRates.Add("57600");
+            ComBaudRates.Add("115200");
+
+            ComDataBits.Add("5");
+            ComDataBits.Add("6");
+            ComDataBits.Add("7");
+            ComDataBits.Add("8");
+
+            ComStopBits.Add("None");
+            ComStopBits.Add("One");
+            ComStopBits.Add("OnePointFive");
+            ComStopBits.Add("Two");
+
+            ComParities.Add("None");
+            ComParities.Add("Even");
+            ComParities.Add("Mark");
+            ComParities.Add("Odd");
+            ComParities.Add("Space");
+
+            ComHandShakes.Add("None");
+            ComHandShakes.Add("XOnXOff");
+            ComHandShakes.Add("RequestToSend");
+            ComHandShakes.Add("RequestToSendXOnxOff");
         }
 
         public ObservableCollection<string> ComPortNames
         {
             get { return _comPortNames; }
             set { _comPortNames = value; }
+        }
+
+        public List<string> ComBaudRates
+        {
+            get { return _comBaudRates; }
+            set { _comBaudRates = value; }
+        }
+
+        public List<string> ComDataBits
+        {
+            get { return _comDataBits; }
+            set { _comDataBits = value; }
+        }
+
+        public List<string> ComStopBits
+        {
+            get { return _comStopBits; }
+            set { _comStopBits = value; }
+        }
+
+        public List<string> ComParities
+        {
+            get { return _comParities; }
+            set { _comParities = value; }
+        }
+
+        public List<string> ComHandShakes
+        {
+            get { return _comHandShakes; }
+            set { _comHandShakes = value; }
         }
 
         public ComPortModel ComPort
