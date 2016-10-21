e46b87acd934cd44eb10df9703aa906fbff1cdcb
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-10-21 13:57:58 +0100

    Refresh button logic and com port name list implemented

--- ComsPortComunicator/ComsPortComunicator/ViewModel/MainWindowViewModel.cs
+++ ComsPortComunicator/ComsPortComunicator/ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,26 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Windows.Input;
 using ComsPortComunicator.Command;
 
 namespace ComsPortComunicator.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
-        public ICommand RefreshPortsCommand { get; set; }
+        private ObservableCollection<string> _comPortNames = new ObservableCollection<string>(); 
 
         public MainWindowViewModel()
         {
             InitCommands();
         }
+
+        public ObservableCollection<string> ComPortNames
+        {
+            get { return _comPortNames; }
+            set { _comPortNames = value; }
+        } 
+
+        public ICommand RefreshPortsCommand { get; set; }
 
         private void InitCommands()
         {
@@ -24,7 +34,12 @@ namespace ComsPortComunicator.ViewModel
 
         private void RefreshPortsCommandExecute()
         {
-            
+            string[] comNames = SerialPort.GetPortNames();
+
+            foreach (string name in comNames)
+            {
+                ComPortNames.Add(name);
+            }
         }
     }
 }
