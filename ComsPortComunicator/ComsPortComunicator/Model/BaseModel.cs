c8bc67f7e20587578c75962f23978dfe933cf6fb
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-10-21 14:28:56 +0100

    fix annotations bug

--- ComsPortComunicator/ComsPortComunicator/Model/BaseModel.cs
+++ ComsPortComunicator/ComsPortComunicator/Model/BaseModel.cs
@@ -1,14 +1,12 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using ComsPortComunicator.Annotations;
 
 namespace ComsPortComunicator.Model
 {
     public class BaseModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
+        
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
