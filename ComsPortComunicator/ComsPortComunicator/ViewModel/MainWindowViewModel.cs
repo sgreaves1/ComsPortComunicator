203733edf9230b04b17da2c8ca36a3863090c713
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-10-21 13:43:10 +0100

    Add refresh ports button and command

---
+++ ComsPortComunicator/ComsPortComunicator/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+using ComsPortComunicator.Command;
+
+namespace ComsPortComunicator.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ICommand RefreshPortsCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            InitCommands();
+        }
+
+        private void InitCommands()
+        {
+            RefreshPortsCommand = new RelayCommand(RefreshPortsCommandExecute, RefreshPortsCommandCanExecute);
+        }
+
+        private bool RefreshPortsCommandCanExecute()
+        {
+            return true;
+        }
+
+        private void RefreshPortsCommandExecute()
+        {
+            
+        }
+    }
+}
