9d129b29e306971b45e0ef8f91d4c9511cb2ebb6
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-10-24 11:57:39 +0100

    Hide send and received section if port is closed

---
+++ ComsPortComunicator/ComsPortComunicator/Converter/PortStatusToVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ComsPortComunicator.Enum;
+
+namespace ComsPortComunicator.Converter
+{
+    public class PortStatusToVisibilityConverter : IValueConverter
+    {
+        enum Direction
+        {
+            Normal, Inverted
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ComOpenState state = (ComOpenState) value;
+            Direction direction = (Direction) System.Enum.Parse(typeof (Direction), (string) parameter);
+
+            bool visible = false;
+
+            switch (state)
+            {
+                case ComOpenState.Open:
+                    visible = true;
+                    break;
+
+                case ComOpenState.Closed:
+                    visible = false;
+                    break;
+            }
+
+            if (direction == Direction.Inverted)
+            {
+                visible = !visible;
+            }
+
+            return visible? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
