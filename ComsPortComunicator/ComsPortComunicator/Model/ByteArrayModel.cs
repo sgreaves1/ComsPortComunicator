00596575dd2fa88d0f83ffaecb3f5d9ba49ebe9c
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-11 12:51:47 +0000

    Add byte array model class

---
+++ ComsPortComunicator/ComsPortComunicator/Model/ByteArrayModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ComsPortComunicator.Model
+{
+    public class ByteArrayModel : BaseModel
+    {
+        private string _name;
+
+        public ByteArrayModel(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
